a79514560e60d9e2c4544e7d66d44fe5a1dbf674
carlos <geral@meuppt.pt>
2021-11-14 06:12:25 +0000

    Woocommerce form helper functions now easier.

--- src/Panels.php
+++ src/Panels.php
@@ -57,12 +57,37 @@ class Panels
 					'type' => 'text'
 				));
 			}
+			if(in_array('number', $form)) {
+				woocommerce_wp_text_input(array(
+					'id' => $form[1] . '_number', 
+					'label' => __($form[2], 'woocommerce'),
+					'desc_tip' => 'true',
+					'description' => __($form[3], 'woocommerce'),
+					'type' => 'number'
+				));
+			}
 			if(in_array('textarea', $form)) {
 				woocommerce_wp_textarea_input(array(
 					'id' => $form[1] . '_textarea', 
 					'label' => __($form[2], 'woocommerce'),
 					'desc_tip' => 'true',
 					'description' => __($form[3], 'woocommerce'),
+				));
+			}
+			if(in_array('select', $form)) {
+				woocommerce_wp_select(array(
+					'id' => $form[1] . '_select', 
+					'label' => __($form[2], 'woocommerce'),
+					'desc_tip' => 'true',
+					'description' => __($form[3], 'woocommerce'),
+					'options' => $form[4]
+				));
+			}
+			if(in_array('radio', $form)) {
+				woocommerce_wp_radio(array(
+					'id' => $form[1] . '_radio', 
+					'label' => __($form[2], 'woocommerce'),
+					'options' => $form[3]
 				));
 			}
 		}
